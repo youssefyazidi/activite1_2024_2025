fb268c03b6ed1241322c575bb7940426fea64b6a
Yazidi Alaoui <yazidi17@gmail.com>
2024-10-15 19:28:06 +0100

    first commit

---
+++ ModeConnecteConnection1/ModeConnecteConnection1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModeConnecteConnection1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            addAccount("Mohamed", 1500, "C");
+            Console.ReadKey();
+        }
+        static void addAccount(string owner, float bal, string atype)
+        {
+            //Chaine de connexion
+            string connectionString =
+              @"Data Source=.\SQLEXPRESS;Integrated Security=true;Initial Catalog=ACCOUNTDB";
+            //Représente une session de travail sur le serveur
+            SqlConnection connexion = new SqlConnection();
+            //Associer la chaine de connexion à l'objet Connection
+            connexion.ConnectionString = connectionString;
+            Console.WriteLine("Database state is : " + connexion.State);
+
+            //parametrer la commande
+
+            string query = "INSERT INTO ACCOUNT(OWNER,BALANCE,AccountType) VALUES ('" + owner + "'," + bal + ",'" + atype + "')";
+            Console.WriteLine(query);
+            SqlCommand commande = new SqlCommand();
+            //definir le code sql de la commande
+            commande.CommandText = query;
+            commande.Connection = connexion;
+
+            //Ouvrir la connexion
+            connexion.Open();
+            Console.WriteLine("Database state is : " + connexion.State);
+
+            int res=commande.ExecuteNonQuery();
+            Console.WriteLine("Nombre de lignes inserées "+ res);
+
+           
+            //Fermer la connexion
+            connexion.Close();
+            Console.WriteLine("Database state is : " + connexion.State);
+
+        }
+    }
+}
---
+++ activite1connectedmode/activite1connectedmode/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace activite1connectedmode
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonCompter = new System.Windows.Forms.Button();
+            this.buttonSupprimer = new System.Windows.Forms.Button();
+            this.buttonAjouter = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // buttonCompter
+            // 
+            this.buttonCompter.Location = new System.Drawing.Point(33, 92);
+            this.buttonCompter.Name = "buttonCompter";
+            this.buttonCompter.Size = new System.Drawing.Size(151, 23);
+            this.buttonCompter.TabIndex = 0;
+            this.buttonCompter.Text = "Compte des personnes";
+            this.buttonCompter.UseVisualStyleBackColor = true;
+            // 
+            // buttonSupprimer
+            // 
+            this.buttonSupprimer.Location = new System.Drawing.Point(33, 150);
+            this.buttonSupprimer.Name = "buttonSupprimer";
+            this.buttonSupprimer.Size = new System.Drawing.Size(151, 23);
+            this.buttonSupprimer.TabIndex = 1;
+            this.buttonSupprimer.Text = "Supprimer la personne";
+            this.buttonSupprimer.UseVisualStyleBackColor = true;
+            // 
+            // buttonAjouter
+            // 
+            this.buttonAjouter.Location = new System.Drawing.Point(33, 213);
+            this.buttonAjouter.Name = "buttonAjouter";
+            this.buttonAjouter.Size = new System.Drawing.Size(151, 23);
+            this.buttonAjouter.TabIndex = 2;
+            this.buttonAjouter.Text = "Ajouter une personne";
+            this.buttonAjouter.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(233, 89);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(181, 147);
+            this.listBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(465, 355);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.buttonAjouter);
+            this.Controls.Add(this.buttonSupprimer);
+            this.Controls.Add(this.buttonCompter);
+            this.Name = "Form1";
+            this.Text = "Connexion";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonCompter;
+        private System.Windows.Forms.Button buttonSupprimer;
+        private System.Windows.Forms.Button buttonAjouter;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
