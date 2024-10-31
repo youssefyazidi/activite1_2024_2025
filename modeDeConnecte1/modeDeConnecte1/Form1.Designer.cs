1a3df7fd9f366a9477334b5d347f15c1e44db7ac
Yazidi Alaoui <yazidi17@gmail.com>
2024-10-31 20:10:18 +0100

    31102024

---
+++ modeDeConnecte1/modeDeConnecte1/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace modeDeConnecte1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.buttonProducts = new System.Windows.Forms.Button();
+            this.buttonSuppliers = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(59, 31);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(531, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // buttonProducts
+            // 
+            this.buttonProducts.Location = new System.Drawing.Point(89, 211);
+            this.buttonProducts.Name = "buttonProducts";
+            this.buttonProducts.Size = new System.Drawing.Size(75, 23);
+            this.buttonProducts.TabIndex = 1;
+            this.buttonProducts.Text = "Products";
+            this.buttonProducts.UseVisualStyleBackColor = true;
+            this.buttonProducts.Click += new System.EventHandler(this.buttonProducts_Click);
+            // 
+            // buttonSuppliers
+            // 
+            this.buttonSuppliers.Location = new System.Drawing.Point(322, 211);
+            this.buttonSuppliers.Name = "buttonSuppliers";
+            this.buttonSuppliers.Size = new System.Drawing.Size(75, 23);
+            this.buttonSuppliers.TabIndex = 2;
+            this.buttonSuppliers.Text = "Suppliers";
+            this.buttonSuppliers.UseVisualStyleBackColor = true;
+            this.buttonSuppliers.Click += new System.EventHandler(this.buttonSuppliers_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(644, 261);
+            this.Controls.Add(this.buttonSuppliers);
+            this.Controls.Add(this.buttonProducts);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Manual DataSet";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button buttonProducts;
+        private System.Windows.Forms.Button buttonSuppliers;
+    }
+}
+
---
+++ modeDeConnecte1/modeDeConnecte1/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace modeDeConnecte1
+{
+    public partial class Form1 : Form
+    {
+        DataSet dataSet;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Creer la base de données locale
+           dataSet = CreateDataSet();
+        }
+
+
+        private DataSet CreateDataSet()
+        {
+            DataSet ds = new DataSet("LocalDb");
+
+            DataTable dataTable;
+            DataColumn dataColumn;
+            DataRow dataRow;
+
+            //Creae la table Products
+            dataTable = new DataTable("Products");
+            ds.Tables.Add(dataTable);
+            //Définir le schema de la table Products
+            dataColumn = new DataColumn("ProductName");
+            dataColumn.DataType = System.Type.GetType("System.String");
+            dataColumn.DefaultValue = "";
+
+            dataTable.Columns.Add(dataColumn);
+
+            dataColumn = new DataColumn("Price");
+            dataColumn.DataType = System.Type.GetType("System.Single");
+            dataColumn.DefaultValue = 0;
+
+            dataTable.Columns.Add(dataColumn);
+
+            dataColumn = new DataColumn("SupplierId");
+            dataColumn.DataType = System.Type.GetType("System.String");
+            dataColumn.DefaultValue = "";
+            dataTable.Columns.Add(dataColumn);
+
+            //Creae la table Suppliers
+            dataTable = new DataTable("Suppliers");
+            ds.Tables.Add(dataTable);
+            //Définir le schema de la table Suppliers
+            dataColumn = new DataColumn("SupplierName");
+            dataColumn.DataType = System.Type.GetType("System.String");
+            dataColumn.DefaultValue = "";
+
+            dataTable.Columns.Add(dataColumn);
+
+            dataColumn = new DataColumn("SupplierId");
+            dataColumn.DataType = System.Type.GetType("System.String");
+            dataColumn.Unique = true;
+            dataTable.Columns.Add(dataColumn);
+
+            //Définir la clé primaire
+            dataTable.PrimaryKey = new DataColumn[]{ dataColumn};
+
+            //Définir la clé étrangère (Relation)
+
+            DataRelation relation = new DataRelation("fk_pr_sup",
+                ds.Tables["Suppliers"].Columns["SupplierId"],
+                ds.Tables["Products"].Columns["SupplierId"]);
+
+            ds.Relations.Add(relation);
+
+            //Creation des donnés
+
+            dataRow = ds.Tables["Suppliers"].NewRow();
+            dataRow["SupplierId"] = "LIMA";
+            dataRow["SupplierName"] = "BEANS,BEANS,BEANS,LCC";
+            ds.Tables["Suppliers"].Rows.Add(dataRow);
+
+            dataRow = ds.Tables["Suppliers"].NewRow();
+            dataRow["SupplierId"] = "ACME";
+            dataRow["SupplierName"] = "ACME Products, Inc.";
+            ds.Tables["Suppliers"].Rows.Add(dataRow);
+
+
+            dataRow = ds.Tables["Products"].NewRow();
+            dataRow["ProductName"] = "Bean Bag Chair";
+            dataRow["Price"] = 38.29;
+            dataRow["SupplierId"] = "LIMA";
+            ds.Tables["Products"].Rows.Add(dataRow);
+
+            dataRow = ds.Tables["Products"].NewRow();
+            dataRow["ProductName"] = "Rocking Chair";
+            dataRow["Price"] = 123.95;
+            dataRow["SupplierId"] = "ACME";
+            ds.Tables["Products"].Rows.Add(dataRow);
+
+            dataRow = ds.Tables["Products"].NewRow();
+            dataRow["ProductName"] = "Bar Stool";
+            dataRow["Price"] = 29.99;
+            dataRow["SupplierId"] = "ACME";
+            ds.Tables["Products"].Rows.Add(dataRow);
+
+            return ds;
+        }
+
+        private void buttonProducts_Click(object sender, EventArgs e)
+        {
+            //Composant consomateur de données
+            dataGridView1.DataSource = dataSet.Tables["Products"];
+        }
+
+        private void buttonSuppliers_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = dataSet;
+            dataGridView1.DataMember = "Suppliers";
+        }
+    }
+}
