1ae863532c9979eb9c331792dfddadc0a7d2d2d8
Yazidi Alaoui <yazidi17@gmail.com>
2024-11-19 19:32:09 +0100

    19112024Form2

---
+++ TP2Partie2/TP2Partie2/Form2.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace TP2Partie2
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxCode = new System.Windows.Forms.TextBox();
+            this.textBoxNom = new System.Windows.Forms.TextBox();
+            this.textBoxAdresse = new System.Windows.Forms.TextBox();
+            this.dateTimePickerInsciption = new System.Windows.Forms.DateTimePicker();
+            this.buttonNouveau = new System.Windows.Forms.Button();
+            this.buttonModifier = new System.Windows.Forms.Button();
+            this.buttonSupprimer = new System.Windows.Forms.Button();
+            this.buttonPremier = new System.Windows.Forms.Button();
+            this.buttonPrecedent = new System.Windows.Forms.Button();
+            this.buttonSuivant = new System.Windows.Forms.Button();
+            this.buttonDernier = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.buttonRechercher = new System.Windows.Forms.Button();
+            this.textBoxCodeRecherche = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.buttonQuitter = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(171, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(216, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mise à jour des adherents";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Code adhérent :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 153);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nom :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(43, 193);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Adresse :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(43, 236);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Date inscription :";
+            // 
+            // textBoxCode
+            // 
+            this.textBoxCode.Location = new System.Drawing.Point(161, 113);
+            this.textBoxCode.Name = "textBoxCode";
+            this.textBoxCode.Size = new System.Drawing.Size(139, 20);
+            this.textBoxCode.TabIndex = 5;
+            // 
+            // textBoxNom
+            // 
+            this.textBoxNom.Location = new System.Drawing.Point(161, 153);
+            this.textBoxNom.Name = "textBoxNom";
+            this.textBoxNom.Size = new System.Drawing.Size(139, 20);
+            this.textBoxNom.TabIndex = 6;
+            // 
+            // textBoxAdresse
+            // 
+            this.textBoxAdresse.Location = new System.Drawing.Point(161, 190);
+            this.textBoxAdresse.Name = "textBoxAdresse";
+            this.textBoxAdresse.Size = new System.Drawing.Size(139, 20);
+            this.textBoxAdresse.TabIndex = 7;
+            // 
+            // dateTimePickerInsciption
+            // 
+            this.dateTimePickerInsciption.Location = new System.Drawing.Point(161, 229);
+            this.dateTimePickerInsciption.Name = "dateTimePickerInsciption";
+            this.dateTimePickerInsciption.Size = new System.Drawing.Size(139, 20);
+            this.dateTimePickerInsciption.TabIndex = 8;
+            // 
+            // buttonNouveau
+            // 
+            this.buttonNouveau.Location = new System.Drawing.Point(375, 105);
+            this.buttonNouveau.Name = "buttonNouveau";
+            this.buttonNouveau.Size = new System.Drawing.Size(181, 23);
+            this.buttonNouveau.TabIndex = 9;
+            this.buttonNouveau.Text = "Nouveau";
+            this.buttonNouveau.UseVisualStyleBackColor = true;
+            // 
+            // buttonModifier
+            // 
+            this.buttonModifier.Location = new System.Drawing.Point(375, 148);
+            this.buttonModifier.Name = "buttonModifier";
+            this.buttonModifier.Size = new System.Drawing.Size(181, 23);
+            this.buttonModifier.TabIndex = 10;
+            this.buttonModifier.Text = "Modifier";
+            this.buttonModifier.UseVisualStyleBackColor = true;
+            // 
+            // buttonSupprimer
+            // 
+            this.buttonSupprimer.Location = new System.Drawing.Point(375, 189);
+            this.buttonSupprimer.Name = "buttonSupprimer";
+            this.buttonSupprimer.Size = new System.Drawing.Size(181, 23);
+            this.buttonSupprimer.TabIndex = 11;
+            this.buttonSupprimer.Text = "Supprimer";
+            this.buttonSupprimer.UseVisualStyleBackColor = true;
+            // 
+            // buttonPremier
+            // 
+            this.buttonPremier.Location = new System.Drawing.Point(46, 273);
+            this.buttonPremier.Name = "buttonPremier";
+            this.buttonPremier.Size = new System.Drawing.Size(48, 23);
+            this.buttonPremier.TabIndex = 12;
+            this.buttonPremier.Text = "<<";
+            this.buttonPremier.UseVisualStyleBackColor = true;
+            // 
+            // buttonPrecedent
+            // 
+            this.buttonPrecedent.Location = new System.Drawing.Point(139, 273);
+            this.buttonPrecedent.Name = "buttonPrecedent";
+            this.buttonPrecedent.Size = new System.Drawing.Size(48, 23);
+            this.buttonPrecedent.TabIndex = 13;
+            this.buttonPrecedent.Text = "<";
+            this.buttonPrecedent.UseVisualStyleBackColor = true;
+            // 
+            // buttonSuivant
+            // 
+            this.buttonSuivant.Location = new System.Drawing.Point(216, 273);
+            this.buttonSuivant.Name = "buttonSuivant";
+            this.buttonSuivant.Size = new System.Drawing.Size(48, 23);
+            this.buttonSuivant.TabIndex = 14;
+            this.buttonSuivant.Text = ">";
+            this.buttonSuivant.UseVisualStyleBackColor = true;
+            // 
+            // buttonDernier
+            // 
+            this.buttonDernier.Location = new System.Drawing.Point(298, 273);
+            this.buttonDernier.Name = "buttonDernier";
+            this.buttonDernier.Size = new System.Drawing.Size(48, 23);
+            this.buttonDernier.TabIndex = 15;
+            this.buttonDernier.Text = ">>";
+            this.buttonDernier.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBoxCodeRecherche);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.buttonRechercher);
+            this.groupBox1.Location = new System.Drawing.Point(356, 245);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Recherche";
+            // 
+            // buttonRechercher
+            // 
+            this.buttonRechercher.Location = new System.Drawing.Point(71, 19);
+            this.buttonRechercher.Name = "buttonRechercher";
+            this.buttonRechercher.Size = new System.Drawing.Size(123, 23);
+            this.buttonRechercher.TabIndex = 10;
+            this.buttonRechercher.Text = "Rechercher";
+            this.buttonRechercher.UseVisualStyleBackColor = true;
+            // 
+            // textBoxCodeRecherche
+            // 
+            this.textBoxCodeRecherche.Location = new System.Drawing.Point(95, 63);
+            this.textBoxCodeRecherche.Name = "textBoxCodeRecherche";
+            this.textBoxCodeRecherche.Size = new System.Drawing.Size(95, 20);
+            this.textBoxCodeRecherche.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 66);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(83, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Code adhérent :";
+            // 
+            // buttonQuitter
+            // 
+            this.buttonQuitter.Location = new System.Drawing.Point(139, 322);
+            this.buttonQuitter.Name = "buttonQuitter";
+            this.buttonQuitter.Size = new System.Drawing.Size(123, 23);
+            this.buttonQuitter.TabIndex = 17;
+            this.buttonQuitter.Text = "Quitter";
+            this.buttonQuitter.UseVisualStyleBackColor = true;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(609, 357);
+            this.Controls.Add(this.buttonQuitter);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.buttonDernier);
+            this.Controls.Add(this.buttonSuivant);
+            this.Controls.Add(this.buttonPrecedent);
+            this.Controls.Add(this.buttonPremier);
+            this.Controls.Add(this.buttonSupprimer);
+            this.Controls.Add(this.buttonModifier);
+            this.Controls.Add(this.buttonNouveau);
+            this.Controls.Add(this.dateTimePickerInsciption);
+            this.Controls.Add(this.textBoxAdresse);
+            this.Controls.Add(this.textBoxNom);
+            this.Controls.Add(this.textBoxCode);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form2";
+            this.Text = "Mise à jour des adherents";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxCode;
+        private System.Windows.Forms.TextBox textBoxNom;
+        private System.Windows.Forms.TextBox textBoxAdresse;
+        private System.Windows.Forms.DateTimePicker dateTimePickerInsciption;
+        private System.Windows.Forms.Button buttonNouveau;
+        private System.Windows.Forms.Button buttonModifier;
+        private System.Windows.Forms.Button buttonSupprimer;
+        private System.Windows.Forms.Button buttonPremier;
+        private System.Windows.Forms.Button buttonPrecedent;
+        private System.Windows.Forms.Button buttonSuivant;
+        private System.Windows.Forms.Button buttonDernier;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBoxCodeRecherche;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button buttonRechercher;
+        private System.Windows.Forms.Button buttonQuitter;
+    }
+}
